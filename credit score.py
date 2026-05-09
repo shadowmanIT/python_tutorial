e0202138046457d2b2ceae4a18a24931a5c32637
luca-72 <luca.andres72@gmail.com>
2026-05-09 12:13:38 -0700

    corrected spacing

--- credit score.py
+++ credit score.py
@@ -4,6 +4,7 @@ has_good_credit = True
 good_down_payment = 1000000 * .10
 bad_down_payment = 1000000 * .20
 
+
 if has_good_credit:
     print(f"You have good credit your down payment needs to be, ${good_down_payment}")
 else:
